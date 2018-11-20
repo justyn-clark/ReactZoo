9b5e0033f3c177b898e25e19c61abc34c2f61b4e
ijustynclark@gmail.com <justyn@justynclark.com>
2018-11-19 18:24:11 -0800

    add styled components and no items message

--- src/components/Animals.js
+++ src/components/Animals.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import AnimalList from './AnimalList'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
 
-const Animals = () => (
+const StyledNoItems = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: black;
+  font-weight: 600;
+  `;
+
+const Animals = (props) => (
     <div className='container__list'>
-        <AnimalList />
+      {!props.animals.length &&
+      <StyledNoItems>
+        <p>No Items Available</p>
+      </StyledNoItems>}
+      {props.animals && <AnimalList {...props}/>}
     </div>
 )
 
-export default Animals
+const mapStateToProps = (state) => {
+  return {
+    animals: state.animals
+  }
+}
+
+export default connect(mapStateToProps)(Animals)
